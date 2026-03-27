cdd9cd2987e7456e691de36afeafe8cbd2283dc7
Jacob Bohlmann <jacobbohlmann45.com>
2026-03-27 15:30:28 -0400

    App Version using JS

---
+++ main.js
@@ -0,0 +1,30 @@
+//main.js
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    mainWindow.loadFile('views/home.html');
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) createWindow();
+});
