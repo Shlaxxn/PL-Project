4f0b985f038a625e006a19861c8f9b5623a79b27
Jacob Bohlmann <jacobbohlmann45.com>
2026-03-27 15:12:15 -0400

    Initial Commit

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 var express = require("express");
 var app = express();
+var dotenv = require('dotenv');
+var path = require("path");
+dotenv.config();
 var HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "public")));
+
 function onHttpStart() {
 console.log("Express http server listening on: " +
 HTTP_PORT);
 }
 app.get("/", function(req,res){
-res.sendFile(__dirname + "/views/home.html");
+res.sendFile(path.join(__dirname, "views/home.html"));
 })
 app.get("/game-stats", function(req,res){
-res.sendFile(path.join(__dirname + "/views/game-stats.html"));
+res.sendFile(path.join(__dirname, "views/game-stats.html"));
 });
 app.get("/deck-stats", function(req,res){
-res.sendFile(path.join(__dirname + "/views/deck-stats.html"));
+res.sendFile(path.join(__dirname, "views/deck-stats.html"));
 });
 app.listen(HTTP_PORT, onHttpStart);
